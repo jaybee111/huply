b0151e1dfe4f48c97511e1c00552de1607db9a42
Jonas Boettcher <jones.boettcher@gmail.com>
2022-03-28 10:48:54 +0200

    Input hidden value edit for fewer request size

--- src/components/FileInputHiddenComponent.ts
+++ src/components/FileInputHiddenComponent.ts
@@ -2,18 +2,20 @@ import AppStore from "../store/AppStore";
 
 class FileInputHiddenComponent {
     private el: HTMLElement;
+    private store: AppStore;
     private input: HTMLInputElement;
 
     constructor(el: HTMLElement, store: AppStore) {
         this.el = el;
+        this.store = store;
         this.input = document.createElement('input');
 
         store.events.subscribe('fileItemUpdate', () => {
-            this.input.setAttribute('value', JSON.stringify(store.getCategorizedFiles()));
+            this.input.setAttribute('value', this.getValue());
         });
 
         store.events.subscribe('fileDeleted', () => {
-            this.input.setAttribute('value', JSON.stringify(store.getCategorizedFiles()));
+            this.input.setAttribute('value', this.getValue());
         });
     }
 
@@ -24,6 +26,24 @@ class FileInputHiddenComponent {
             this.input.setAttribute('name', nameAttr);
         }
         return this.input;
+    }
+
+    getValue() {
+        const categorizedFiles = this.store.getCategorizedFiles();
+        categorizedFiles.uploaded = categorizedFiles.uploaded.map((fileItem) => {
+            fileItem.data = undefined;
+            return fileItem;
+        });
+        categorizedFiles.deleted = categorizedFiles.deleted.map((fileItem) => {
+            fileItem.data = undefined;
+            return fileItem;
+        });
+        categorizedFiles.preloaded = categorizedFiles.preloaded.map((fileItem) => {
+            fileItem.url = undefined;
+            return fileItem;
+        });
+
+        return JSON.stringify(categorizedFiles);
     }
 }
 
